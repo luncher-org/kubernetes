097d9eedc1b9a46d2f02c30aa3cd3c026ab00646
vardhaman22 <vardhaman.surana@suse.com>
2024-06-05 21:12:47 +0530

    use container stats from cadvisor for cri-dockerd

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -378,7 +378,6 @@ func PreInitRuntimeService(kubeCfg *kubeletconfiginternal.KubeletConfiguration, 
 	}
 
 	kubeDeps.useLegacyCadvisorStats = cadvisor.UsingLegacyCadvisorStats(kubeCfg.ContainerRuntimeEndpoint)
-
 	return nil
 }
 
@@ -764,6 +763,7 @@ func NewMainKubelet(kubeCfg *kubeletconfiginternal.KubeletConfiguration,
 	hostStatsProvider := stats.NewHostStatsProvider(kubecontainer.RealOS{}, func(podUID types.UID) string {
 		return getEtcHostsPath(klet.getPodDir(podUID))
 	}, podLogsDirectory)
+	klog.InfoS("stats provider: ", "useLegacyCadvisorStats", kubeDeps.useLegacyCadvisorStats)
 	if kubeDeps.useLegacyCadvisorStats {
 		klet.StatsProvider = stats.NewCadvisorStatsProvider(
 			klet.cadvisor,
